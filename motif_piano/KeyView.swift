b10b9afb388f6af9c266a05ed6c813cb04781125
CecilioN7 <navarroscecilio@gmail.com>
2024-09-02 01:53:09 -0700

    Keyboard layout

--- motif_piano/KeyView.swift
+++ motif_piano/KeyView.swift
@@ -1,28 +1,33 @@
-//
-//  KeyView.swift
-//  motif_piano
-//
-//  Created by Cecilio Samuel Navarro on 9/1/24.
-//
-
+// KeyView.swift
 import SwiftUI
 
 let NOTES = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 
 struct KeyView: View {
+    var note: String
+    var isBlack: Bool
+    
     var body: some View {
-        Rectangle()
-            .fill(Color.white)
-            .frame(width: 60, height: 300)
-            .border(Color.black, width: 2)
-//        Text("Key!")
-//            .font(.headline)
-//            .padding()
-//            .background(Color.gray)
-//            .cornerRadius(5.0)
+        ZStack {
+            Rectangle()
+                .fill(isBlack ? Color.black : Color.white)
+                .frame(width: isBlack ? 40 : 60, height: isBlack ? 200 : 300)
+                .border(Color.black, width: 20)
+            if !isBlack {
+                Text(note)
+                    .foregroundColor(.black)
+                    .bold()
+                    .padding(.top, 200)
+            }
+        }
+        .zIndex(isBlack ? 1 : 0) // Make sure black keys appear on top
     }
 }
 
 #Preview {
-    KeyView()
+    Group {
+        KeyView(note: "C", isBlack: false)
+        KeyView(note: "Db", isBlack: true)
+    }
 }
+
--- motif_piano/PianoView.swift
+++ motif_piano/PianoView.swift
@@ -1,20 +1,39 @@
-//
-//  PianoView.swift
-//  motif_piano
-//
-//  Created by Cecilio Samuel Navarro on 9/1/24.
-//
-
+// PianoView.swift
 import SwiftUI
 
 struct PianoView: View {
     var body: some View {
-        HStack {
-            KeyView()
-            KeyView()
-            KeyView()
+        ZStack(alignment: .topLeading) {
+            HStack(spacing: -0) {
+                ForEach(NOTES.filter { !$0.contains("b") }, id: \.self) { note in
+                    KeyView(note: note, isBlack: false)
+                }
+            }
+            HStack(spacing: 0) {
+                ForEach(NOTES.filter { $0.contains("b") }, id: \.self) { note in
+                    KeyView(note: note, isBlack: true)
+                        .offset(x: getOffset(for: note))
+                }
+            }
         }
         .padding()
+    }
+    
+    private func getOffset(for note: String) -> CGFloat {
+        switch note {
+        case "Db":
+            return 30
+        case "Eb":
+            return 70
+        case "Gb":
+            return 130
+        case "Ab":
+            return 160
+        case "Bb":
+            return 190
+        default:
+            return 0
+        }
     }
 }
 
