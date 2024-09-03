2709fe3ecefa5e177c5f9b9138823d4d9def323b
CecilioN7 <navarroscecilio@gmail.com>
2024-09-03 15:55:08 -0700

    added chord recognizer

---
+++ motif_piano/ChordRecognizer.swift
@@ -0,0 +1,8 @@
+//
+//  ChordRecognizer.swift
+//  motif_piano
+//
+//  Created by Cecilio Samuel Navarro on 9/2/24.
+//
+
+import Foundation
---
+++ motif_piano/ChordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChordView.swift
+//  motif_piano
+//
+//  Created by Cecilio Samuel Navarro on 9/2/24.
+//
+
+import SwiftUI
+
+struct ChordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChordView()
+}
